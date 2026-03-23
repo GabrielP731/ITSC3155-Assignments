450ecfebbb71ec60ddbd3d0e827a6ae053fef498
Gabriel Pacheco <gpacheco0731@gmail.com>
2026-03-23 15:03:16 -0400

    Sandwiches.py

---
+++ Assignment5/api/controllers/sandwiches.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from ..models import models, schemas
+
+def create(db: Session, sandwich: schemas.SandwichCreate):
+    db_sandwich = models.Sandwich(
+        sandwich_name=sandwich.sandwich_name,
+        price=sandwich.price
+    )
+    db.add(db_sandwich)
+    db.commit()
+    db.refresh(db_sandwich)
+    return db_sandwich
+
+def read_all(db: Session):
+    return db.query(models.Sandwich).all()
+
+def read_one(db: Session, sandwich_id: int):
+    return db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id).first()
